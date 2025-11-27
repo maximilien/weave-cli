505e07426d3e8279847c4077aa91f42ead109c83
Michael Maximilien <mmaximilien@gmail.com>
2025-11-27 11:38:55 -0800

    refactor: rename Windows stubs to unsupported, add ARM64 support
    - Rename *_windows.go files to *_unsupported.go for clarity
    - Expand build tags to exclude Chroma on Windows OR ARM64 platforms
    - Chroma's libtokenizers CGO dependency doesn't work on Windows or ARM64
    - More generic naming allows for easier addition of other unsupported platforms
    - Maintains same functionality but with clearer, more extensible naming
    
    Signed-off-by: Michael Maximilien <mmaximilien@gmail.com>

--- src/cmd/utils/vectordb_client_chroma.go
+++ src/cmd/utils/vectordb_client_chroma.go
@@ -1,5 +1,5 @@
-//go:build !windows
-// +build !windows
+//go:build linux && amd64
+// +build linux,amd64
 
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2025 dr.max
@@ -7,6 +7,6 @@
 package utils
 
 import (
-	// Import Chroma adapter to register its factory (non-Windows only)
+	// Import Chroma adapter to register its factory (Linux AMD64 only)
 	_ "github.com/maximilien/weave-cli/src/pkg/vectordb/chroma"
 )
---
+++ src/cmd/utils/vectordb_client_chroma_unsupported.go
@@ -0,0 +1,9 @@
+//go:build !linux || !amd64
+// +build !linux !amd64
+
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025 dr.max
+
+package utils
+
+// Chroma adapter only available on Linux AMD64 due to libtokenizers CGO dependency
