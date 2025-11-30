75022cf85f018f974bd206db14acfaca658f83c1
Michael Maximilien <mmaximilien@gmail.com>
2025-11-30 11:50:20 -0800

    fix: exclude NEXT_STEPS*.md files from markdown linting
    These files are working documents and don't need to follow strict markdown rules.
    
    Signed-off-by: Michael Maximilien <mmaximilien@gmail.com>

--- lint.sh
+++ lint.sh
@@ -175,7 +175,7 @@ if find . -name "*.md" -not -path "./src/vendor/*" -not -path "./node_modules/*"
             ./tools/fix_markdown_lint.sh > /dev/null 2>&1
         fi
 
-        if markdownlint "**/*.md" --ignore node_modules --ignore src/vendor --ignore docs --ignore "WORK_PLAN_*.md" --ignore "SESSION_SUMMARY_*.md" --ignore "TODOs-*.md"; then
+        if markdownlint "**/*.md" --ignore node_modules --ignore src/vendor --ignore docs --ignore "WORK_PLAN_*.md" --ignore "SESSION_SUMMARY_*.md" --ignore "TODOs-*.md" --ignore "NEXT_STEPS*.md"; then
             print_success "Markdown linting passed!"
         else
             print_warning "Markdown linting issues found"
